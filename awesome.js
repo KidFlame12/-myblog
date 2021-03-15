2085c2f0eea48b03f1f223e3f08c2ca6f66dd801
KidFlame12 <78328100+KidFlame12@users.noreply.github.com>
2021-03-15 18:05:30 -0400

    Add files via upload

---
+++ awesome.js
@@ -0,0 +1,35 @@
+
+//ADD YOUR FIREBASE LINKS HERE
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+      function adduser() {
+
+            user_name = document.getElementById("user_name").value;
+        
+            localStorage.setItem("user_name", user_name);
+        
+            window.location = "kwitter_room.html";
+        }
+function addroom()  
+{
+      Room_name = document.getElementById("Room_name").value;
+      firebase.database().ref("/").child(Room_names)
+}      
+      //End code
+      });});}
+getData();
+ function send() {
+     msg = document.getElementById("msg").value;
+     firebase.database().ref(Room_name).push({
+           name:user_name,
+           message:msg,
+           like:0
+     });
+
+     document.getElementById("msg").value= "";
+ }
+                    
+ 
+ 
