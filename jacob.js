477de206850d196448716e46b31d83791f92248b
KidFlame12 <78328100+KidFlame12@users.noreply.github.com>
2021-04-30 21:27:00 -0400

    Add files via upload

---
+++ jacob.js
@@ -0,0 +1,12 @@
+webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('camera');
+
+function take_snapshot()
