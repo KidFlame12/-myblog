7d7cea149313132870b263beff032159c67f48a7
KidFlame12 <78328100+KidFlame12@users.noreply.github.com>
2021-05-20 13:31:54 -0400

    Add files via upload

--- bigbrain.js
+++ bigbrain.js
@@ -18,3 +18,36 @@ Webcam.set({
         document.log
         })
     }
+    function gotResults(error, results) {
+        if(error) {
+            console.error(error);
+        } else {
+            console.log(results)
+            document.getElementById("result_sign_name").innerHTML = results[0].label;
+            document.getElementById("result_sign_name2").innerHTML = results[1].label;
+        } if(results[0].label == "peace")
+        {
+            document.getElementById("update_sign").innerHTML = "U+270C";
+        }
+        if(results[0].label == "ok")
+        {
+            document.getElementById("update_sign").innerHTML = "U+1F44C";
+        }
+        if(results[0].label == "thumbs up")
+        {
+            document.getElementById("update_sign").innerHTML = "U+1F44D;";
+        }
+   
+        if(results[1].label == "peace")
+        {
+            document.getElementById("update_sign").innerHTML = "U+270C";
+        }
+        if(results[1].label == "ok")
+        {
+            document.getElementById("update_sign").innerHTML = "U+1F44C";
+        }
+        if(results[1].label == "thumbs up")
+        {
+            document.getElementById("update_sign").innerHTML = "U+1F44D";
+        }
+    }
