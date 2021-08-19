ecd518ad08546df103b46bbde5a9ec580744004a
KidFlame12 <78328100+KidFlame12@users.noreply.github.com>
2021-08-19 18:06:09 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,106 +1,20 @@
-function setup() {
-    canvas = createCanvas(600, 500)
-    canvas.center();
+img = "";
 
-
-    video = createCanvas(VIDEO);
-    video.hide();
-
-    poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on('pose', gotPoses);
+function preload() {
+    img = loadimage('dog_cat.jpg')
 }
+
+function setup() {
+    canvas = createCanvas(640, 420)
+    canvas.center();
+}
+
 
 function draw() {
-   image(video, 0, 0, 600, 500); 
-
-
-   fill("#00FC05")
-   stroke("#07E8FF")
-   circle(rightWrist,rightWristY,20);
-
-   if(rightWristY >0 && rightWristY <= 100)
-   {
-       document.getElementById("speed").innerHTML = "Speed = 0.5x"
-       song.rate(0.5);
-    }
-    else if(rightWristY >100 && rightWristY <= 200)
-   {
-       document.getElementById("speed").innerHTML = "Speed = 1x"
-       song.rate(1);
-    }
-    else if(rightWristY >200 && rightWristY <= 300)
-   {
-       document.getElementById("speed").innerHTML = "Speed = 1.5x"
-       song.rate(1.5);
-    }
-    else if(rightWristY >300 && rightWristY <= 400)
-   {
-       document.getElementById("speed").innerHTML = "Speed = 2x"
-       song.rate(2);  
-    }
-    else if(rightWristY >400 && rightWristY <= 500)
-   {
-       document.getElementById("speed").innerHTML = "Speed = 2.5"
-       song.rate(2.5);  
-    }
-   if(scoreLeftWrist > 0.2)
-   {
-       circle(leftWristX,leftWristY,20);
-       InNumberleftWristY = Number(leftWristY);
-       remove_decimals = floor(InNumberleftWristY);
-       volume = remove_decimals/500;
-       document.getElementById("volume").innerHTML = "Volume = " + volume;
-       song.setVolume(volume);
-   }
-} 
-
-song = ""; 
-
-
-scoreRightWrist = 0;
-scoreLeftWrist = 0;
-
-
-leftWristX = 0;
-rightWristX = 0;
-
-
-leftWristY = 0;
-rightWristY = 0;
-
-
-function preload()
-{
-    song = loadSound("music.mp3")   
-}
-
-function play() {
-   song.play;
-   song.setvolume(1);
-   song.rate(1); 
-}
-
-function modelLoaded() {
-    console.log('PoseNet is Initiliazed');
-}
-
-function gotPoses(results)
-{
-    if(results.length > 0)
-    {
-        console.log(results)
-        scoreRightWrist = results[0].pose.keypoints[10].score;
-        scoreLeftWrist = results[0].pose.keypoints[9].score;
-        console.log("scoreLeftWrist = " + scoreLeftWrist);
-
-        
-        rightWristX = results[0].pose.rightWrist.x
-        rightWristX = results[0].pose.rightWrist.y
-        console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY);
-
-
-        leftWristX = results[0].pose.leftWrist.x
-        leftWristX = results[0].pose.leftWrist.y
-        console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
-    }
+    image(img, 0, 0, 640, 420);
+    fill("FF0000");
+    text("dog", 45, 75);
+    nofill();
+    stroke("FF0000");
+    rect(30, 6, 450, 350 );
 }
