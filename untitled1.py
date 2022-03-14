6fd6f7a5dfaf2b27c62ca282bd1bb366e23ca6e3
KidFlame12 <78328100+KidFlame12@users.noreply.github.com>
2022-03-14 19:19:32 -0400

    Add files via upload

--- untitled1.py
+++ untitled1.py
@@ -9,6 +9,8 @@ Created on Thu Feb 24 20:17:53 2022
 from tkinter import *
 import speech__recoginition as sr
 import pyttsx3
+import webbbrowser
+import subprocess
 from datetime import datetime
 
 root = Tk()
@@ -26,7 +28,8 @@ def r_audio():
     try:
         voice_data= speech_recognisor.recognize_google(audio, language='en-in')
     except sr.UnkownValueError:
-        print('Please repeat i did not get that')
+        print('Please repeat I did not get that')
+        speak('Please repeat I did not get that')
     print(voice_data)
 
 def respond(voice_data):
@@ -42,5 +45,22 @@ def respond(voice_data):
         current_time = now.strftime("%H:%M:%S")
         speak(current_time)
         print(current_time)
-    r_audio()
-    root.mainloop
+     
+    if "search" in voice_data:
+        speak("Opening Google")
+        print("Opening Google")
+        webbrowser.get().open("https://www.google.com/")
+        
+    if "video" in voice_data:
+            speak("Opening Youtube")
+            print("Opening Youtube")
+            webbrowser.get().open("https://www.youtube.com/")
+    if "text editor" in voice_data:
+        speak("Opening Notes")
+        print("Opening Notes")
+        webbrowser.get().open(["notes.exe"])
+btn= Button(root, text="Start",bg="red3",fg="white", padx=10, pady=1,font=("Arial",11,"bold"), relief=FLAT, command=r_audio)
+btn.place(relx=0.5,rely=0.5,anchor=CENTER)
+
+
+root.mainloop()
