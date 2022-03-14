0cc326cef11819d1640ee803544f272a83ae4e2f
KidFlame12 <78328100+KidFlame12@users.noreply.github.com>
2022-03-14 19:20:34 -0400

    Add files via upload

--- untitled5.py
+++ untitled5.py
@@ -1,6 +1,33 @@
-from tkinter import *
-root=Tk()
-root.title("Dictionary")
-root.geometry("600x400")
+from tkinter import *  
+from tkinter import ttk
+import psutil
+from psutil._common import BatteryTime
+root = Tk()
+root.geometry('500x250')
+root.config(bg="black")
+root.overridedirect(True)
+style = ttk.Style(root)
+style.layout('ProgressBarStyle',[('Horizantal.Progressbar.trough',{'children': [('Horizantal.Progressbar.pbar',{'side',:'right','sticky': 'ns'})],'sticky': 'nsew'}),('Horizantal.Progressbar.label', {'sticky': ''})])
+bar = ttk.Progressbar(root, maximum=100, style='ProgressBarStyle')
+bar.place(relx=0.5, rely=0.2.anchor=CENTER)
+battery_life = Label(root, font = 'arial 15 bold', bg ='black', fg="white")
+battery_life.place(relx=0.5,rely=0.5, anchor=CENTER)
+def converTime(seconds):
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    return str(hours)+" : "+str(seconds)
 
-label_of_mutable = Label()
+def getBatterylife():
+    battery = psutil.sensors_battery()
+    bar['value'] = battery.percent
+    style.configure('ProgressBarStyle', text=str(battery.percent)+' %')
+    battery_left = convertTime(battery.secsleft)
+    if battery.secsleft == BatteryTime.POWER_TIME_UNLIMITED:
+        battery_life['text'] = 'Unplug the battery! \n And run the code again.'
+    elif battery.secsleft['text'] == BatteryTime().POWER_TIME_UNKNOWN:
+        battery_life['text'] = 'Battery life not detected. \n Please, run the code again!'
+    else:
+        battery_life['text'] ="Battery Life : "+battery_left
+        root.after(1000, getBatterylife)
+getBatterylife()
+root.mainloop()        
--- untitled6.py
+++ untitled6.py
@@ -1,49 +1,26 @@
-from tkinter import *
-from PIL import ImageTk, Image
-import random
-
-root=Tk()
-root.title("Endless Dice Game")
-root.geometry("600x600")
-root.configure(background="yellow2")
-
-img=ImageTk.PhotoImage(Image.open ("dice.4.jpg"))
-
-player1 = Label(root,text = "Player 2", bg = "royal blue", fg = "white")
-player1.place(relx = 0.1, rely = 0.3 , anchor = CENTER)
-
-player2 = Label(root,text = "Player 2", bg = "royal blue", fg = "white")
-player2.place(relx = 0.9, rely = 0.3 , anchor = CENTER)
-
-player_1_score_label = Label(root, bg = "royal blue", fg = "white")
-player_1_score_label.place(relx = 0.1, rely = 0.4 , anchor = CENTER)
-
-player_2_score_label = Label(root, bg = "royal blue", fg = "white")
-player_2_score_label.place(relx = 0.9, rely = 0.4 , anchor = CENTER)
-
-random_dice_label = Label(root,bg = "chocolate1", fg = "white")
-random_dice_label.place(relx = 0.5, rely = 0.5 , anchor = CENTER)
-
-player_1_score = 0
-def player1():
-    global player1_score
-    random_no = random.randit(1,6)
-    random_dice_label["text"] = "Player 1 Dice Random Number : " +str(random_no)
-    player1_score = player1_score + random_no
-    player_1_score_label["text"] = (player1_score)
-    
-    player_1_btn = Button(root, image = img, command = player1)
-    player_1_btn.place(relx = 0.1, rely = 0.6 , anchor = CENTER)
-    
-    player_2_score = 0
-def player2():
-    global player2_score
-    random_no = random.randit(1,6)
-    random_dice_label["text"] = "Player 2 Dice Random Number : " +str(random_no2)
-    player2_score = player2_score + random_no2
-    player_2_score_label["text"] = (player2_score)
-    
-    player_2_btn = Button(root, image = img, command = player2)
-    player_2_btn.place(relx = 0.1, rely = 0.6 , anchor = CENTER)
-
-root.mainloop
+import hashlib
+from simplecrypt import encrypt, decrypt
+value = "Peter : Hello"
+def SHA256():
+    result = hashlib.sha256(value.encode())
+    print("SHA256 encrypted data : ",result.hexdigest())
+SHA256()
+def MD5():
+    result = hashlib.MD5(value.encode())
+    print("MD5 encrypted data : ",result.hexdigest())
+MD5()
+MESSAGE = "Peter : Hello"
+hex_string = ''
+def encryption():
+    global hex_string
+    ciphercode = encrypt('AIM', message)
+    hex_string = ciphercode.hex()
+    print("Encryption " , hex_stringdef encryption()
+def decryption():
+        global hex_string
+        byte_str = bytes.fromhex(hex_string)
+        original = decrypt('AIM,' byte_str)
+        final_message = original.decode("utf-8")
+        print("Decryption " , final_message)
+encryption()
+decryption()
