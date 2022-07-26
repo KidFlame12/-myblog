b8e3f96ac5487fb65e8117475ea6b3f8e0b0c004
KidFlame12 <78328100+KidFlame12@users.noreply.github.com>
2022-07-26 19:35:15 -0400

    Add files via upload

--- untitled16.py
+++ untitled16.py
@@ -1,33 +1,16 @@
-Class Doctor():
-    
-    def __init__(self):
-        print("This is class Doctor")
-        
-    def BMI(weight, height):
-        bmi = weight/(height*height)
-        print("Your BMI is "+str(bmi))
-        
-    def heart_rate(heart_rates):
-        if(heart_rates)>60 and heart_rate<(100):
-            print("Great your heart rate is normal")
-        else:
-            print("your heart rate does not seem normal, please visit the clinic")
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 21 20:10:36 2022
 
-class Patient(Doctor):
-    
-    def __init__(self, name, eweight, height, heart_rates):
-        self.patient_name = name
-        self.paitent_weight = weight
-        self.paitent_height = height
-        self.paitent_heart_rates = heart_rates
-        
-    def HealthCheck(self):
-        print("\n Paitent Name: ", self.paitent_name)
-        Doctor.BMI(self.paitent_weight, self.paitent_height)
-        Doctor.heart_rate(self.paitent_heart_rate)
-        
-paitent1 = Paitent("Michael", 30, 0.9144, 80)
-paitent1.healthCheck()
+@author: Bradley
+"""
 
-paitent2 = Paitent("Jessica", 40, 1, 120)
-paitent2.healthCheck
+for nonce in : # set a range for iteration
+	equation = 25-5+nonce
+	if equation == 24:
+		print() #display as "verified" and mention the nonce value at which it got verified. 
+		break
+	else:
+		print()#display as " not verified"
+<?xml version="1.0" encoding="UTF-8"?>
