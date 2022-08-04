7b97d5791a31d0be6d03c4a24df1e1120e6da49b
KidFlame12 <78328100+KidFlame12@users.noreply.github.com>
2022-08-03 20:39:24 -0400

    Add files via upload

--- untitled18.py
+++ untitled18.py
@@ -1,18 +1,21 @@
-import matplotlib .pyplot as plt
-import psutil as p
-app_name_list = []
-app_name_percantage_list = []
-count = 0
-for process in p.process_iter():
-    count = count +1
-    if count <= 6:
-        name = process.name()
-        cpu_usage = p.cpu_percent()
-        print('name : ', name,'-- cpu_usage : ', cpu_usage)
-        app_name_list.append(name)
-        app_name_percentage_list.append(cpu_usage)
-plt.figure(figsize=(15,7))
-plt.xlabel("Application")
-plt.ylabel("usage")
-plt.bar(app_name_list, app_name_percantage_list,width=0.6,color=("purple","green","red","blue","orange","pink"))
-plt.show
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 28 20:16:23 2022
+
+@author: Bradley
+"""
+
+import hashlib
+PB_current_hash = "573de6af99199bdc7ae9534891d512afbc2b1473f2f6a5784e0c078d67a60bf9"
+
+str = "Alice sends James 5 Eth amount"
+
+result = hashlib.sha256(str.encode())
+CB_previous_hash = result.hexdigest()
+
+
+if PB_current_hash==CB_previous_hash:
+    print("Hach Verified...!")
+else:
+    print("Hash not Verified")
